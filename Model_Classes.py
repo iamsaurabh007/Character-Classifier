95e8736c31ed0b23b66008d43f7e28d5b629b76b
iamsaurabh007 <saurabh.yadav1947@gmail.com>
2021-03-23 17:11:34 +0530

    channel

--- Model_Classes.py
+++ Model_Classes.py
@@ -65,7 +65,7 @@ class Symbol_Model(nn.Module):
         super(Symbol_Model,self).__init__()
         self.conv_block1=Conv_block()
         self.conv_block2=Conv_block(out_chnl*6)
-        self.conv1=nn.Conv2d(in_channels=out_chnl*36,out_channels=config.num_classes,kernel_size=1)
+        self.conv1=nn.Conv2d(in_channels=out_chnl*6,out_channels=config.num_classes,kernel_size=1)
     
     def forward(self,x):
         x=self.conv_block1(x)
